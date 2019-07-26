ef34273f020b0c89de53afe60d42829895f01926
BenR <20133902+thisbenrogers@users.noreply.github.com>
2019-07-26 10:40:09 -0500

    :lipstick: Adds Tabs and Labels to Nav

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { Tab, Menu, Icon } from 'semantic-ui-react'
 import { NavLink } from "react-router-dom";
 
-// TODO: Add missing tabs below
-
 const Nav = props => (
 	<NavLink
 		exact
@@ -16,10 +14,14 @@ const createLabel = (iconName, labelText) => <span><Icon name={iconName} />{labe
 
 const welcomeLabel = createLabel("home", "Home Page")
 const characterLabel = createLabel("users", "Characters")
+const locationLabel = createLabel("map outline", "Locations")
+const episodeLabel = createLabel("video", "Episodes")
 
 const panes = [
-  { menuItem: <Menu.Item key='home' as={Nav} to={`/`} content={welcomeLabel} /> },
-  { menuItem: <Menu.Item key='characters' as={Nav} to={`/characters`} content={characterLabel} /> }
+	{ menuItem: <Menu.Item key='home' as={Nav} to={`/`} content={welcomeLabel} /> },
+	{ menuItem: <Menu.Item key='characters' as={Nav} to={`/characters`} content={characterLabel} /> },
+	{ menuItem: <Menu.Item key='locations' as={Nav} to={`/locations`} content={locationLabel} /> },
+	{ menuItem: <Menu.Item key='episodes' as={Nav} to={`/episodes`} content={episodeLabel} /> }
 ]
 
 const TabNav = () => <Tab panes={panes} renderActiveOnly={false} />
